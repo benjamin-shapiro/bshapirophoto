ddbfd709e2cdc0640633d7a8fd41ba437eafdaff
Ben <bps78@cornell.edu>
2021-09-23 01:54:18 -0400

    testing

--- pages/ithaca.js
+++ pages/ithaca.js
@@ -1,8 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
-import Nav from "../components/Nav.js"
+import Nav from "../components/Nav.js";
 
-import astrobike from "../public/ithaca/Astro Bike_.jpg";
 import beebesouth from "../public/ithaca/Beebe South Long.jpg";
 import beebelong from "../public/ithaca/Beebe Wide 2_.jpg";
 import foresthome from "../public/ithaca/Forest Home Intersection Night.jpg";
@@ -14,9 +13,11 @@ import triphammer from "../public/ithaca/Trippin Hammer Swirl 2_.jpg";
 export default function Ithaca() {
   return (
     <div>
-    <div><Nav/></div>
+      <div>
+        <Nav />
+      </div>
       <h1>Ithaca</h1>
-      <Image src={astrobike} placeholder="blur"></Image>
+
       <Image src={beebesouth} placeholder="blur"></Image>
       <Image src={beebelong} placeholder="blur"></Image>
       <Image src={foresthome} placeholder="blur"></Image>
