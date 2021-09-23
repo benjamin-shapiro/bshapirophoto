c67579613b4c22bb4731c6d44cba69fa00bfd464
Ben <bps78@cornell.edu>
2021-09-23 01:56:24 -0400

    more testing

--- pages/ithaca.js
+++ pages/ithaca.js
@@ -2,29 +2,6 @@ import Head from "next/head";
 import Image from "next/image";
 import Nav from "../components/Nav.js";
 
-import beebesouth from "../public/ithaca/Beebe South Long.jpg";
-import beebelong from "../public/ithaca/Beebe Wide 2_.jpg";
-import foresthome from "../public/ithaca/Forest Home Intersection Night.jpg";
-import fuertes from "../public/ithaca/Fuertes Black and White_.jpg";
-import mtpleasantroad from "../public/ithaca/Mt. Pleasant Night_.jpg";
-import taughannockcloud from "../public/ithaca/Taughannock Cloudy_.jpg";
-import triphammer from "../public/ithaca/Trippin Hammer Swirl 2_.jpg";
-
 export default function Ithaca() {
-  return (
-    <div>
-      <div>
-        <Nav />
-      </div>
-      <h1>Ithaca</h1>
-
-      <Image src={beebesouth} placeholder="blur"></Image>
-      <Image src={beebelong} placeholder="blur"></Image>
-      <Image src={foresthome} placeholder="blur"></Image>
-      <Image src={fuertes} placeholder="blur"></Image>
-      <Image src={mtpleasantroad} placeholder="blur"></Image>
-      <Image src={taughannockcloud} placeholder="blur"></Image>
-      <Image src={triphammer} placeholder="blur"></Image>
-    </div>
-  );
+  return <div></div>;
 }
