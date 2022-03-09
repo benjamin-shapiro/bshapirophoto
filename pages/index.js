25e5207597c8a1a429ef1c9a515868ba73b2cc2e
Benjamin Shapiro <bps78@cornell.edu>
2022-03-09 17:56:40 -0500

    added favicon set

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,9 @@ export default function Homescreen() {
         Benjamin Shapiro Ithaca, bshapirophoto, nashcalc, Astrophotography, \
         Travel Photography"
         />
+          <link rel="icon" type="image/png" sizes="32x32" href="/eclipse32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/eclipse16.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/eclipse-apple-touch.png" />
       </Head>
 
       <div>
