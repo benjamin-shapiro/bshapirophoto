5a455a2be860ea15eaecd52f47e6790171db69bd
Benjamin Shapiro <bps78@cornell.edu>
2024-05-14 10:54:51 -0700

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -9,7 +9,6 @@ export default function Footer() {
   return (
     <div className={footerStyles.socials}>
       <SocialIcon url="https://www.linkedin.com/in/benjaminshapiro1/" target="_blank"/>
-      <SocialIcon url="https://www.instagram.com/bshapirophoto/" target="_blank"/>
       <SocialIcon url="https://benjamin-shapiro.github.io/" target="_blank"/>
       <SocialIcon url="mailto:bps78@cornell.edu" target="bps78@cornell.edu"/>
     </div>
